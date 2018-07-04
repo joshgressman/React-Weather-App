5942316000e6ea049b082167a560c661e1af7004
joshgressman <joshgressman@gmail.com>
2018-07-04 12:56:14 -0500

    added method and bind

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+
+  // Initialize State
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+
+    //Bind contect of input changed
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  //Event handles all DOM event change handlers
+  onInputChange(event) {
+   console.log(event.target.value);
+   this.setState({term: event.target.value});
+  }
+
   render() {
     return (
       <form className="input-group">
-       <input />
+       <input 
+        className="form-control"
+        placeholder="Get a five day forcast in you city"
+        value={this.state.term}
+        //Callback / Higher order function function, function passed to another function
+        onChange={this.onInputChange}
+       />
        <span className="input-group-btn">
         <button type="submit" className="btn btn-secondary">Submit</button>
        </span>
